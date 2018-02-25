3bcf28c3a07b0d731ede39964726fd8dd974341e
Kana Natsuno <dev@whileimautomaton.net>
2018-02-25 19:22:31 +0900

    Add a server skeleton

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.set('port', process.env.PORT || 5000)
+
+app.get('/', (request, response) => {
+  response.send('Hello')
+})
+
+app.listen(app.get('port'), () => {
+  console.log('Node app is running on port', app.get('port'))
+})
