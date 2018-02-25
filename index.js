c781d499609e5bcb90ffae43895789dcef998ad0
Kana Natsuno <dev@whileimautomaton.net>
2018-02-25 19:49:25 +0900

    Respond to challenge

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 
 const app = express()
 
+app.use(bodyParser.json())
+
 app.set('port', process.env.PORT || 5000)
 
-app.get('/', (request, response) => {
-  response.send('Hello')
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
+
+app.post('/', (req, res) => {
+  res.send(req.body && req.body.challenge)
 })
 
 app.listen(app.get('port'), () => {
