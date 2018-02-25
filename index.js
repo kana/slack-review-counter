5c0d03e5251cdb3c04706ae25b3a0ba44a0e356c
Kana Natsuno <dev@whileimautomaton.net>
2018-02-25 22:11:55 +0900

    Implement status updater

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+const slack = require('slack')
 
 const VERIFICATION_TOKEN = process.env.VERIFICATION_TOKEN
+const OAUTH_ACCESS_TOKEN = process.env.OAUTH_ACCESS_TOKEN
 
 const app = express()
 
@@ -13,7 +15,7 @@ app.get('/', (req, res) => {
   res.send('Hello')
 })
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
   if (!req.body) {
     res.end()
     console.log('missing body')
@@ -29,8 +31,69 @@ app.post('/', (req, res) => {
   if (req.body.challenge) {
     return res.send(req.body.challenge)
   }
+
+  if (req.body.event && req.body.event.type === 'message') {
+    res.end()
+    await handleMessage(req.body.event.text || '')
+    return
+  }
+
+  res.end()
 })
 
 app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'))
 })
+
+async function handleMessage (text) {
+  if (!text.match(/\b(kana|@kana|U1UP30NRL)\b/)) {
+    return
+  }
+
+  const moreCount = (text.match(/https:\/\/github.com\/pixiv\/[^\/]+\/pull\/\d+/g) || []).length
+
+  const p = await usersProfileGet()
+  const statusText = p.profile.status_text || ''
+
+  const m = statusText.match(/\d+/g)
+  const prevCount = m ? parseInt(m[0], 10) : 0
+  const currentDay = new Date().getDay()
+  const prevDay = m && m[1] ? parseInt(m[1], 10) : currentDay
+
+  const newCount = currentDay === prevDay ? prevCount + moreCount : moreCount
+
+  const emojiTable = {
+    0: ':zero:',
+    1: ':one:',
+    2: ':two:',
+    3: ':three:',
+    4: ':four:',
+    5: ':five:',
+    6: ':six:',
+    7: ':seven:',
+    8: ':eight:',
+    9: ':nine:',
+    10: ':keycap_ten:'
+  }
+  const emoji = emojiTable[newCount] || ':waving_white_flag:'
+
+  usersProfileSet({
+    status_emoji: emoji,
+    status_text: newCount + '個レビューしてる(#' + currentDay + ')'
+  })
+}
+
+async function usersProfileGet ()
+{
+  return await slack.users.profile.get({
+    token: OAUTH_ACCESS_TOKEN
+  });
+}
+
+async function usersProfileSet (statusData)
+{
+  return await slack.users.profile.set({
+    token: OAUTH_ACCESS_TOKEN,
+    profile: JSON.stringify(statusData)
+  });
+}
